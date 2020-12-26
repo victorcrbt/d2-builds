bac9628cf1c132c5ac6bf4882eff62969624ee3a
Victor Batalha <madivcb@gmail.com>
2020-12-26 02:55:14 -0300

    fix: eslint fixes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 
 export interface IAppProps {}
 
-const App: React.FC<IAppProps> = () => {
-  return <h1>Hello, World!</h1>;
-}
+const App: React.FC<IAppProps> = () => <h1>Hello, Worlds!</h1>;
 
 export default App;
