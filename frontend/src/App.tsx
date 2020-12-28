6ab438613d5ec99297aad7b96808340f4375d41d
Victor Batalha <madivcb@gmail.com>
2020-12-27 21:03:37 -0300

    feat: created app router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,54 +1,29 @@
-import React, { useCallback } from 'react';
+import React from 'react';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { ThemeProvider, DefaultTheme } from 'styled-components';
-import { useTranslation } from 'react-i18next';
+import { Router } from 'react-router-dom';
 
 import '@config/Reactotron';
-import i18n from '@i18n/index';
 import { store, persistor } from '@store/index';
 
-import {
-  PORTUGUESE_LANGUAGE,
-  ENGLISH_LANGUAGE,
-} from '@constants/i18n/languages';
-import { CHANGE_LANGUAGE } from '@constants/i18n/general';
 import usePersisTheme from '@hooks/usePersistTheme';
 
-import Hello from '@components/Hello';
+import Routes from './router';
+import history from './services/history';
+
 import darkTheme from './styles/themes/dark';
-import ligthTheme from './styles/themes/light';
 
 const App: React.FC = () => {
-  const [theme, setTheme] = usePersisTheme<DefaultTheme>(darkTheme);
-
-  const { t } = useTranslation();
-
-  const changeLanguage = useCallback(
-    () =>
-      i18n.changeLanguage(
-        i18n.language === PORTUGUESE_LANGUAGE
-          ? ENGLISH_LANGUAGE
-          : PORTUGUESE_LANGUAGE
-      ),
-    []
-  );
-
-  const toggleTheme = useCallback(
-    () => {
-      setTheme(theme.name === 'light' ? darkTheme : ligthTheme);
-    },
-    []
-  )
+  const [theme] = usePersisTheme<DefaultTheme>(darkTheme);
 
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor}>
         <ThemeProvider theme={theme}>
-          <Hello />
-
-          <button onClick={changeLanguage}>{t(CHANGE_LANGUAGE)}</button>
-          <button onClick={toggleTheme}>trocar tema</button>
+          <Router history={history}>
+            <Routes />
+          </Router>
         </ThemeProvider>
       </PersistGate>
     </Provider>
